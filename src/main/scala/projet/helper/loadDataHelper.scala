fa66f1eb8fdaab84df6624de2d7e90be0ffad51b
Celinevialard <celine.vialard@gmail.com>
2023-06-09 15:57:18 +0200

    load csv

--- src/main/scala/projet/helper/loadDataHelper.scala
+++ src/main/scala/projet/helper/loadDataHelper.scala
@@ -19,3 +19,31 @@ def choosePostalCode (city:String, location:String, postalCode:String): PostalCo
     }
   }
 }
+
+
+def loadCsv (): Seq[Property] =
+{
+  var properties: Seq[Property] = Seq()
+  //Load data (CV)
+  try{
+  val csvFile = "./08-PropertiesLondon.csv"
+  val reader = CSVReader.open(new File(csvFile))
+      properties = reader.allWithHeaders().map(record => 
+      Property(record("Property Name"), 
+        record("Price").toInt,
+        HouseType.fromString(record("House Type")).getOrElse(HouseType.House), 
+        record("Area in sq ft").toInt,
+        record("No. of Bedrooms").toInt,
+        record("No. of Bathrooms").toInt,
+        record("No. of Receptions").toInt,
+        choosePostalCode(record("Location"),record("City/County"),record("Postal Code"))
+        )
+      )
+  properties
+  }
+  catch {
+    case ex:Exception =>
+      ex.printStackTrace
+      System.exit(1)
+  }
+}

e762549477c8e4f9452a52ec51504c18c6e8a8de
Celinevialard <celine.vialard@gmail.com>
2023-06-10 11:17:26 +0200

    add tostring

--- src/main/scala/projet/class/City.scala
+++ src/main/scala/projet/class/City.scala
@@ -1,3 +1,5 @@
 package projet
 
-case class City(val city:String, val postalCode: String) extends PostalCodeTrait with CityTrait
+case class City(val city:String, val postalCode: String) extends PostalCodeTrait with CityTrait{
+        override def toString: String = s"City: $city, PostalCode: $postalCode"
+}

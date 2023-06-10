d676cc2360f37656e87289b7e0f1570ceb4a545b
Celinevialard <celine.vialard@gmail.com>
2023-06-10 15:24:23 +0200

    add doc

--- src/main/scala/projet/helper/loadDataHelper.scala
+++ src/main/scala/projet/helper/loadDataHelper.scala
@@ -4,54 +4,62 @@ import java.io.FileReader
 import java.io.File
 
 object DataHelper {
-def choosePostalCode (city:String, location:String, postalCode:String): PostalCodeTrait =
-{
-  if(city!=null && city!="")
+  /**
+    * Choisie la bonne classe à implémenter en fonction des valeurs entrées
+    *
+    * @param city
+    * @param location
+    * @param postalCode
+    * @return
+    */
+  def choosePostalCode (city:String, location:String, postalCode:String): PostalCodeTrait =
   {
-    if(location!=null&& location!=""){
-      new Address(city,location,postalCode)
-    }
-    else{
-      new City(city,postalCode)
-    }
-  }else{
-    if(location!=null&& location!=""){
-      new Location(location,postalCode)
-    }
-
-    //TODO à vérifier, car on peut pas instancier directement un trait
-    else{
-      new PostalCode(postalCode)
+    if(city!=null && city!="")
+    {
+      if(location!=null&& location!=""){
+        new Address(city,location,postalCode)
+      }
+      else{
+        new City(city,postalCode)
+      }
+    }else{
+      if(location!=null&& location!=""){
+        new Location(location,postalCode)
+      }
+      else{
+        new PostalCode(postalCode)
+      }
     }
   }
-}
 
-
-def loadCsv (): Seq[Property] =
-{
-  var properties: Seq[Property] = Seq()
-  //Load data (CV)
-  try{
-  val csvFile = "./08-PropertiesLondon.csv"
-  val reader = CSVReader.open(new File(csvFile))
+  /**
+    * Charge les propriétés du fichier csv 
+    * @return
+    */
+  def loadCsv (): Seq[Property] =
+  {
+    var properties: Seq[Property] = Seq()
+    //Load data (CV)
+    try{
+      val csvFile = "./08-PropertiesLondon.csv"
+      val reader = CSVReader.open(new File(csvFile))
       properties = reader.allWithHeaders().map(record => 
-      Property(record("Property Name"), 
-        record("Price").toInt,
-        HouseTypeEnum.fromString(record("House Type")).getOrElse(HouseTypeEnum.House), 
-        record("Area in sq ft").toInt,
-        record("No. of Bedrooms").toInt,
-        record("No. of Bathrooms").toInt,
-        record("No. of Receptions").toInt,
-        choosePostalCode(record("City/County"),record("Location"),record("Postal Code"))
+        Property(record("Property Name"), 
+          record("Price").toInt,
+          HouseTypeEnum.fromString(record("House Type")).getOrElse(HouseTypeEnum.House), 
+          record("Area in sq ft").toInt,
+          record("No. of Bedrooms").toInt,
+          record("No. of Bathrooms").toInt,
+          record("No. of Receptions").toInt,
+          choosePostalCode(record("City/County"),record("Location"),record("Postal Code"))
         )
       )
-  properties
+    }
+    catch {
+      case ex:Exception =>
+        ex.printStackTrace
+        System.exit(1)
+    }
+    properties
   }
-  catch {
-    case ex:Exception =>
-      ex.printStackTrace
-      System.exit(1)
-      Seq()
-  }
-}
 }
--- src/main/scala/projet/helper/queryHelper.scala
+++ src/main/scala/projet/helper/queryHelper.scala
@@ -2,59 +2,82 @@ package projet
 import java.{util => ju}
 
 object Queries{
-/**
-  * affiche le prix par mètre carré de chaque propriété
-  *
-  * @param properties
-  * @return
-  */
-def query1 (properties:Seq[Property]) : Seq[(String, Double, HouseTypeEnum, PostalCodeTrait)] = {
-   try{
-        properties.map(property => (property.name, property.price/property.area.toDouble, property.houseType, property.address)).sortBy(_._2).take(10)
-    }
-    catch {
-        case ex:Exception =>
-        ex.printStackTrace
-        Seq()
-    }
-}
-
-def query2(properties: Seq[Property]): Seq[(String, Long)] = {
-  if (properties.nonEmpty) {
-    properties.groupBy(_.houseType).map {
-      case (houseType, properties) =>
-        val totalPrices = properties.foldLeft(0L)((total, property) => total + property.price)
-        val avgPriceByProperty = totalPrices / properties.size
-        (HouseTypeEnum.toString(houseType), avgPriceByProperty)
-    }.toSeq.sortBy(_._2)
-  } else {
-    Seq.empty
+  /**
+    * affiche le prix par mètre carré de chaque propriété
+    *
+    * @param properties
+    * @return
+    */
+  def query1 (properties:Seq[Property]) : Seq[(String, Double, HouseTypeEnum, PostalCodeTrait)] = {
+    try{
+          properties.map(property => (property.name, property.price/property.area.toDouble, property.houseType, property.address)).sortBy(_._2).take(10)
+      }
+      catch {
+          case ex:Exception =>
+          ex.printStackTrace
+          Seq()
+      }
   }
-}
 
-def query3 (properties: Seq[Property], filter: HouseTypeEnum) : Seq[Property] = {
-    properties.filter(_.houseType == filter)
-}
-
-
-def query4(properties: Seq[Property], area: Int) : Seq[(String, Double)] = {
-  properties
-    .filter(property => property.area > area)
-    .map(property => (property.name, property.nbBedrooms + property.nbReceptions + (0.5 * property.nbBathrooms)))
-    .sortBy(_._2)
-}
-
-
-def query5 (properties: Seq[Property], cityName: String) : Seq[(HouseTypeEnum, Int)] = {
-    val cityProperties = properties.filter { property =>
-        property.address match {
-            case cityTrait: CityTrait => cityTrait.city.equals(cityName)
-            case _ => false
-        }
+  /**
+    * Donne le prix moyen par type de maison
+    *
+    * @param properties
+    * @return
+    */
+  def query2(properties: Seq[Property]): Seq[(String, Long)] = {
+    if (properties.nonEmpty) {
+      properties.groupBy(_.houseType).map {
+        case (houseType, properties) =>
+          val totalPrices = properties.foldLeft(0L)((total, property) => total + property.price)
+          val avgPriceByProperty = totalPrices / properties.size
+          (HouseTypeEnum.toString(houseType), avgPriceByProperty)
+      }.toSeq.sortBy(_._2)
+    } else {
+      Seq.empty
     }
-    val countByHouseType = cityProperties.groupBy(_.houseType).mapValues(_.size)
-    countByHouseType.toSeq
-}
+  }
 
+  /**
+    * Liste les propriétés par type de maison
+    *
+    * @param properties
+    * @param filter
+    * @return
+    */
+  def query3 (properties: Seq[Property], filter: HouseTypeEnum) : Seq[Property] = {
+      properties.filter(_.houseType == filter)
+  }
 
+  /**
+    *  Donne le nombre de pièce pondérer par propriété
+    *
+    * @param properties
+    * @param area
+    * @return
+    */
+  def query4(properties: Seq[Property], area: Int) : Seq[(String, Double)] = {
+    properties
+      .filter(property => property.area > area)
+      .map(property => (property.name, property.nbBedrooms + property.nbReceptions + (0.5 * property.nbBathrooms)))
+      .sortBy(_._2)
+  }
+
+  /**
+    * Donne le nombre de type de maison pour une ville
+    *
+    * @param properties
+    * @param cityName
+    * @return
+    */
+  def query5 (properties: Seq[Property], cityName: String) : Seq[(HouseTypeEnum, Int)] = {
+      val cityProperties = properties.filter { property =>
+          property.address match {
+              case cityTrait: CityTrait => cityTrait.city.equals(cityName)
+              case _ => false
+          }
+      }
+      val countByHouseType = cityProperties.groupBy(_.houseType).mapValues(_.size)
+      countByHouseType.toSeq
+  }
 }

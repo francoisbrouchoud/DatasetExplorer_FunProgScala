41ba400f348f673cd851db44c7825efa2d768f02
Celinevialard <celine.vialard@gmail.com>
2023-06-10 13:37:23 +0200

    fix future ?

--- src/main/scala/projet/Projet.scala
+++ src/main/scala/projet/Projet.scala
@@ -1,5 +1,7 @@
 package projet
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import com.github.tototoshi.csv._
 import java.io.FileReader
 import java.io.File
@@ -11,43 +13,51 @@ import java.{util => ju}
   println(s"Welcom in our application show properties. Please wait data are loading")
 
   future.onComplete
-{ properties =>
-  //Query 1
-  val futureQr1 = Future(Queries.query1(properties))
-  futureQr1.onComplete
-  { resultQry1 => 
-    var index = 1
-    println(s"Query 1 : Top ten properties by price per area")
-    resultQry1.foreach { case (name, priceM2, houseType, address) =>
-      println(s"-------------------------------")
-      println(s"Property ${index}: $name")
-      println(s"Price per area : ${"%.2f".format(priceM2)} £/m2")
-      println(s"Type: $houseType")
-      println(s"Address: $address")
-      index +=1
+  {  
+    case scala.util.Failure(ex) => println(s"Erreur : ${ex.getMessage}")
+    case scala.util.Success(properties) => {
+      //Query 1
+      val futureQr1 = Future(Queries.query1(properties))
+      futureQr1.onComplete
+      {  
+        case scala.util.Failure(ex) => println(s"Erreur : ${ex.getMessage}")
+        case scala.util.Success(resultQry1) => {
+          var index = 1
+          println(s"Query 1 : Top ten properties by price per area")
+          resultQry1.foreach { case (name, priceM2, houseType, address) =>
+            println(s"-------------------------------")
+            println(s"Property ${index}: $name")
+            println(s"Price per area : ${"%.2f".format(priceM2)} £/m2")
+            println(s"Type: $houseType")
+            println(s"Address: $address")
+            index +=1
+          }
+        }
+      }
+
+      //Query 2
+      val futureQr2 = Future(Queries.query2(properties))
+      futureQr2.onComplete
+      { 
+        case scala.util.Failure(ex) => println(s"Erreur : ${ex.getMessage}")
+        case scala.util.Success(resultQry2) => {
+          println(s"\n***********************************\n")
+          println(s"Query 2 : Average price by house type")
+          resultQry2.foreach { case (houseType, avgPriceByProperty) => 
+            println(s"Average price for $houseType = $avgPriceByProperty £")
+          }
+        }
+      }
     }
+
+
+
+  // Queries.query3(properties, HouseTypeEnum.Duplex)
+  // Queries.query4(properties)
+  // Queries.query5(properties)
+
+
   }
-
-  //Query 2
-  val futureQr2 = Future(Queries.query2(properties))
-  futureQr2.onComplete
-  { resultQry2 =>
-    println(s"\n***********************************\n")
-    println(s"Query 2 : Average price by house type")
-    resultQry2.foreach { case (houseType, avgPriceByProperty) => 
-          println(s"Average price for $houseType = $avgPriceByProperty £")
-    }
-  }
-
-
-
-
- // Queries.query3(properties, HouseTypeEnum.Duplex)
- // Queries.query4(properties)
- // Queries.query5(properties)
-
-
-}
 /*
 var properties: Seq[Property] = Seq()
 //Load data (CV)

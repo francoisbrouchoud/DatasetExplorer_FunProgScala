058f36897f1da15ce317d306beed49410a9f4776
Celinevialard <celine.vialard@gmail.com>
2023-06-10 08:29:08 +0200

    fix properties class

--- src/main/scala/projet/class/Property.scala
+++ src/main/scala/projet/class/Property.scala
@@ -1,6 +1,6 @@
 package projet
 
-case class Property(val name:String, val price:Int, val houseType:HouseTypeEnum, val area:Int, val nbBedrooms:Int, val nbBathrooms:Int, val nbReceptions:Int, val address:PostalCode)
+case class Property(val name:String, val price:Int, val houseType:HouseTypeEnum, val area:Int, val nbBedrooms:Int, val nbBathrooms:Int, val nbReceptions:Int, val address:PostalCodeTrait)
 {
   override def toString: String = s"House(name: $name, houseType: $houseType, price: $price)"
 }
